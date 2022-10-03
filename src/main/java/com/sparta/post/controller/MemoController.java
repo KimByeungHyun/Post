0a6c75d3cd737d77116a0ddd82a5a5dc3b660701
KimByeungHyun <qudgus05@naver.com>
2022-10-04 00:09:09 +0900

    spring_boot_backend_server

--- src/main/java/com/sparta/post/controller/MemoController.java
+++ src/main/java/com/sparta/post/controller/MemoController.java
@@ -18,29 +18,34 @@ public class MemoController {
     private final MemoRepository memoRepository;
     private final MemoService memoService;
 
-    @GetMapping("/api/memos") // 전체 게시물 조회
+    @GetMapping("/api/memos") // 전체 게시물 조회 API (제목, 작성자명, 작성 날짜)
     public List<Memo> getMemos() {
         return memoRepository.findAllByOrderByModifiedAtDesc();
     }
 
-    @PostMapping("/api/memos") // 게시글 등록
+    @PostMapping("/api/memos") // 게시글 작성 API (제목, 작성자명, 비밀번호, 작성 내용)
     public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
         Memo memo = new Memo(requestDto);
         return memoRepository.save(memo);
     }
 
-    @GetMapping("/api/memos/{id}")
+    @GetMapping("/api/memos/{id}") // 게시글 조회 API (제목, 작성자명, 작성 날짜, 작성 내용)
     public Optional<Memo> getMemos(@PathVariable Long id) {
         return memoRepository.findById(id);
     }
 
-    @PutMapping("/api/memos/{id}")
+    @PostMapping("/api/momos/{id}") //비밀번호 확인 API (비밀번호 일치여부 확인))
+    public String pwcheck(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        return memoService.pwCheck(id,requestDto);
+    }
+
+    @PutMapping("/api/memos/{id}") // 게시글 수정 API (제목, 작성자명, 비밀번호, 작성 내용)
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
         memoService.update(id, requestDto);
         return id;
     }
 
-    @DeleteMapping("/api/memos/{id}")
+    @DeleteMapping("/api/memos/{id}") // 게시글 삭제 API (글 삭제)
     public Long deleteMemo(@PathVariable Long id) {
         memoRepository.deleteById(id);
         return id;
